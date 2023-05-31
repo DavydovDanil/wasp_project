3892640e52a0bc2842fb092ae37b2621c33c2de6
dan77 <dan776@bk.ru>
2023-05-31 12:40:54 +0300

    token updated

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import telebot
-import sqlite3
 from telebot import types
+
 bot = telebot.TeleBot('6047835028:AAHha2Rn-1_THc9tEpSwvRaVn4N65qDZohI')
 
+#added default menu
 @bot.message_handler(commands=['start'])
-
 def start(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
     organizer = types.KeyboardButton('Я организатор')
@@ -15,10 +15,11 @@ def start(message):
                            reply_markup=markup)
     bot.register_next_step_handler(msg, user_answer)
 
+#exponation menu
 def user_answer(message):
     if message.text == 'Я организатор':
         bot.send_message(message.chat.id, 'Введите ваш пароль')
-        #bot.register_next_step_handler(msgge, ImOrganiser)
+        # bot.register_next_step_handler(msgge, ImOrganiser)
     elif message.text == 'Я ученик':
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
         back = types.KeyboardButton('Назад')
@@ -35,12 +36,19 @@ def user_answer(message):
         msge = bot.send_message(message.chat.id, 'Бот нужен для помощи в регистрации на курсы WASP Academy',
                                 reply_markup=markup)
         bot.register_next_step_handler(msge, step1)
-#def ImOrganiser(message):
-    #bot.send_message(chat_id)
+
+
+# def ImOrganiser(message):
+# bot.send_message(chat_id)
+
+# if student menu clicked
 def ImStudent(message):
     msg = bot.send_message(message.chat.id,
                            'Вы были перенесены на этап регистрации. Введите фамилию, имя, отчество (Пример: Кисляков Антон Юрьевич)')
     bot.register_next_step_handler(msg, step22)
+
+
+#exception if user made a mistake
 def step1(message):
     if (message.text == 'Назад'):
         start(message)
@@ -48,6 +56,8 @@ def step1(message):
         bot.send_message(message.chat.id, 'Вы нажали нечто не то')
         start(message)
 
+
+#exception in name menu
 def step2(message):
     if (message.text == 'Назад'):
         start(message)
@@ -60,21 +70,25 @@ def step2(message):
         bot.send_message(message.chat.id, 'Вы нажали что-то не то, и вас перебросило')
         start(message)
 
+
+#confirmation after name menu
 def step22(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
     yes = types.KeyboardButton('Да')
     no = types.KeyboardButton('Нет')
     back = types.KeyboardButton('Назад')
     markup.add(yes, no, back)
-    msg = bot.send_message(message.chat.id, f"Ваши имя, фамилия, отчество: {message.text}, вы уверены?",reply_markup=markup)
+    msg = bot.send_message(message.chat.id, f"Ваши имя, фамилия, отчество: {message.text}, вы уверены?",
+                           reply_markup=markup)
 
     bot.register_next_step_handler(msg, step23)
 
-def step23 (message):
 
+#just exception
+def step23(message):
     if message.text == 'Да':
         gorod(message)
-    elif  message.text == 'Нет':
+    elif message.text == 'Нет':
         ImStudent(message)
     elif message.text == 'Назад':
         bot.send_message(message.chat.id, "Вы вернулись в меню")
@@ -82,17 +96,19 @@ def step23 (message):
         msg = bot.send_message(message.chat.id, "Вы написали что-то не то и вас перебросило в начальное меню")
         start(message)
 
-def gorod(message):
 
+#user chooses city
+def gorod(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
     back = types.KeyboardButton('Назад')
     markup.add(back)
     msg = bot.send_message(message.chat.id,
-                     'В каком городе ты живёшь? \nНапример: Москва \n(Обрати внимание, что курс проходит очно в Москве)')
+                           'В каком городе ты живёшь? \nНапример: Москва \n(Обрати внимание, что курс проходит очно в Москве)')
     bot.register_next_step_handler(msg, proverka_goroda)
 
-def proverka_proverki_goroda (message):
 
+#city exception
+def proverka_proverki_goroda(message):
     if message.text == 'Нет':
         gorod(message)
     elif message.text == 'Да':
@@ -103,14 +119,17 @@ def proverka_proverki_goroda (message):
         msg = bot.send_message(message.chat.id, "Выбери команду из меню")
         gorod(msg)
 
+
+#city exception exception
 def proverka_goroda(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
     no = types.KeyboardButton('Нет')
     yes = types.KeyboardButton('Да')
     back = types.KeyboardButton('Назад')
     markup.add(yes, no, back)
-    msg = bot.send_message(message.chat.id, f"Ваш город: {message.text}, вы уверены?",reply_markup=markup)
+    msg = bot.send_message(message.chat.id, f"Ваш город: {message.text}, вы уверены?", reply_markup=markup)
     bot.register_next_step_handler(msg, proverka_proverki_goroda)
+
 
 def phone(message):
     msg = bot.send_message(message.chat.id, 'Введите номер телефона \n(Например "8800555353")')
@@ -126,8 +145,8 @@ def proverka_phone(message):
     msg = bot.send_message(message.chat.id, f"Ваш город: {message.text}, вы уверены?", reply_markup=markup)
     bot.register_next_step_handler(msg, proverka_proverki_phone)
 
-def proverka_proverki_phone(message):
 
+def proverka_proverki_phone(message):
     if message.text == 'Нет':
         phone(message)
     elif message.text == 'Да':
@@ -138,9 +157,11 @@ def proverka_proverki_phone(message):
         msg = bot.send_message(message.chat.id, "Выбери команду из меню")
         phone(msg)
 
+
 def vvedite_pochtu(message):
     msg = bot.send_message(message.chat.id, 'Введите почту \n(Например "kislyakovanton@lit1533.com")')
     bot.register_next_step_handler(msg, proverka_pochtu)
+
 
 def proverka_pochtu(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
@@ -150,6 +171,7 @@ def proverka_pochtu(message):
     markup.add(yes, no, back)
     msg = bot.send_message(message.chat.id, f"Ваша почта: {message.text}, вы уверены?", reply_markup=markup)
     bot.register_next_step_handler(msg, proverka_proverki_pochtu)
+
 
 def proverka_proverki_pochtu(message):
     if message.text == 'Нет':
@@ -162,14 +184,17 @@ def proverka_proverki_pochtu(message):
         msg = bot.send_message(message.chat.id, "Выбери команду из меню")
         vvedite_pochtu(msg)
 
+
 def Vvedenie_K_Testu(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
     no = types.KeyboardButton('Нет')
     yes = types.KeyboardButton('Да')
     back = types.KeyboardButton('Назад')
     markup.add(yes, no, back)
-    msg = bot.send_message(message.chat.id, 'Теперь тебе нужно пройти тест, который будет длиться 1 час\nТы готов?',reply_markup=markup)
+    msg = bot.send_message(message.chat.id, 'Теперь тебе нужно пройти тест, который будет длиться 1 час\nТы готов?',
+                           reply_markup=markup)
     bot.register_next_step_handler(msg, proverka_gotovnosti_k_testu)
+
 
 def proverka_gotovnosti_k_testu(message):
     if message.text == 'Нет':
@@ -182,19 +207,19 @@ def proverka_gotovnosti_k_testu(message):
         msg = bot.send_message(message.chat.id, "Выбери команду из меню")
         Vvedenie_K_Testu(msg)
 
-@bot.message_handler(content_types=["text"])
 
+@bot.message_handler(content_types=["text"])
 def send_help(message):
     bot.send_message(message.chat.id, 'Напишите /start, чтобы использовать бота')
+
 
 def step_back(message):
     start(message)
 
-@bot.message_handler(func=lambda message: message.text == 'Назад', content_types=['text'])
 
+@bot.message_handler(func=lambda message: message.text == 'Назад', content_types=['text'])
 def step2_back_handler(message):
     start(message)
-
 
 
 bot.polling(none_stop=True)
