c49e50e116e5a34ed4c501427dc102426694cc93
dan77 <dan776@bk.ru>
2023-06-02 10:24:04 +0300

    добавлен метод, создающий бд и нечто заполняющее бд

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 import telebot
+import sqlite3
 from telebot import types
+
+def databasecreation():
+    sqlite_connection = sqlite3.connect('kislyakovdatabase.db')
+    sqlite_create_table_query = '''CREATE TABLE student_info(
+                                    id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+                                    name TEXT NOT NULL,
+                                    surname TEXT NOT NULL,
+                                    patronymic TEXT,
+                                    email TEXT NOT NULL,
+                                    phone_number INTEGER NOT NULL,
+                                    first_stage_result TEXT,
+                                    second_stage_result TEXT,
+                                    school TEXT NOT NULL,
+                                    city TEXT NOT NULL,
+                                    status TEXT NOT NULL);'''
+
+    cursor = sqlite_connection.cursor()
+    cursor.execute(sqlite_create_table_query)
+    sqlite_connection.commit()
+    cursor.close()
+
+
+def databasefill_studentinfo(a, b, c):
+    sqlite_connection = sqlite3.connect('kislyakovdatabase.db')
+    sqlite_insert_query = """INSERT INTO student_info
+                              (id, name, surname,patronymic,email,phone_number,first_stage_result)
+                              VALUES (""" + a + """, 'Alex', 'sale@gmail.com', '2020-11-20', 8600);"""
 
 bot = telebot.TeleBot('6047835028:AAHha2Rn-1_THc9tEpSwvRaVn4N65qDZohI')
 
-#added default menu
 @bot.message_handler(commands=['start'])
+
 def start(message):
+    databasecreation()
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
     organizer = types.KeyboardButton('Я организатор')
     uchenik = types.KeyboardButton('Я ученик')
@@ -26,7 +55,7 @@ def user_answer(message):
         ready = types.KeyboardButton('Готов(a) регистрироваться!')
         markup.add(ready, back)
         msg = bot.send_message(message.chat.id,
-                               'Поступление на наши курсы состоит из 3-ёх этапов: \n\n1) Заполнение анкеты\n2) Тест в онлайн-формате\n 3) Тест в очном формате\n 4) Интервью',
+                               'Поступление на наши курсы состоит из 4-ёх этапов: \n\n1) Заполнение анкеты\n2) Тест в онлайн-формате\n3) Тест в очном формате\n4) Интервью',
                                reply_markup=markup)
         bot.register_next_step_handler(msg, step2)
     elif message.text == 'Зачем нужен бот?':
@@ -81,6 +110,7 @@ def step22(message):
     msg = bot.send_message(message.chat.id, f"Ваши имя, фамилия, отчество: {message.text}, вы уверены?",
                            reply_markup=markup)
 
+
     bot.register_next_step_handler(msg, step23)
 
 
@@ -132,7 +162,7 @@ def proverka_goroda(message):
 
 
 def phone(message):
-    msg = bot.send_message(message.chat.id, 'Введите номер телефона \n(Например "8800555353")')
+    msg = bot.send_message(message.chat.id, 'Введите номер телефона \n(Например "88005553535" или \n"+78005553535")')
     bot.register_next_step_handler(msg, proverka_phone)
 
 
@@ -221,5 +251,7 @@ def step_back(message):
 def step2_back_handler(message):
     start(message)
 
+
+conn = sqlite3.connect('kislyakov.db', check_same_thread=False)
 
 bot.polling(none_stop=True)
